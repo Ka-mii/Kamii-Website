8644aa2af4368d5d1c48a5b8a03ed87647bad2fd
Ka-mii <micki.zhang@hotmail.fr>
2022-11-14 02:14:29 +0100

    test

--- Kamii_Website_Project/Kamii_Website_Project/wsgi.py
+++ Kamii_Website_Project/Kamii_Website_Project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Kami_Website_Project project.
+WSGI config for Kamii_Website_Project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kami_Website_Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kamii_Website_Project.settings')
 
 application = get_wsgi_application()
